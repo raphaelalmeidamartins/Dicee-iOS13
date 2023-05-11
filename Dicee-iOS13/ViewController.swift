d81b0d1b8f1591d8bf1ad167d8fece33b168155c
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-05-11 10:58:24 -0300

    :sparkles: feat: challenge 3 completed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1Outlet: UIImageView!
+    @IBOutlet weak var diceImageView2Outlet: UIImageView!
+    
+    var leftDiceImageIndex = 0;
+    var rightDiceImageIndex = 5;
+    
+    let diceSideImageLiterals = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix"),
+    ];
 
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view.
-        
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix");
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo");
+        diceImageView1Outlet.image = diceSideImageLiterals[leftDiceImageIndex];
+        diceImageView2Outlet.image = diceSideImageLiterals[rightDiceImageIndex];
     }
 
-    @IBAction func handleRollButtonPress(_ sender: UIButton) {
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour");
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour");
+    @IBAction func handleRollButtonPressAction(_ sender: UIButton) {
+        leftDiceImageIndex += 1;
+        rightDiceImageIndex -= 1;
+
+        diceImageView1Outlet.image = diceSideImageLiterals[leftDiceImageIndex];
+        diceImageView2Outlet.image = diceSideImageLiterals[rightDiceImageIndex];
     }
 }
